2682dfe51996c650cbc75431244fe2d30f163af6
dominikfladung <dominik.fladung@gmx.de>
2021-12-02 08:23:05 +0100

    java day2

--- java/Day2.java
+++ java/Day2.java
@@ -2,14 +2,52 @@ public class Day2 extends Day {
 
     @Override
     int part1(String[] puzzleInput) {
-        // TODO Auto-generated method stub
-        return 0;
+        int depth = 0;
+        int horizontalPosition = 0;
+
+        for (String line : puzzleInput) {
+            String[] split = line.split(" ");
+            int units = Integer.parseInt(split[1]);
+            String direction = split[0].trim();
+            
+            if(direction.equals("forward")) {
+                horizontalPosition += units;
+            } else if(direction.equals("up")) {
+                depth -= units;
+            } else if(direction.equals("down")) {
+                depth += units;
+            }
+        }        
+
+        return depth * horizontalPosition;
     }
 
     @Override
     int part2(String[] puzzleInput) {
-        // TODO Auto-generated method stub
-        return 0;
+        int depth = 0;
+        int horizontalPosition = 0;
+        int aim = 0;
+
+        for (String line : puzzleInput) {
+            String[] split = line.split(" ");
+            int units = Integer.parseInt(split[1]);
+            String direction = split[0].trim();
+            
+            if(direction.equals("forward")) {
+                horizontalPosition += units;
+                depth += units * aim;
+            } else if(direction.equals("up")) {
+                aim -= units;
+            } else if(direction.equals("down")) {
+                aim += units;
+            } else {
+                System.out.println("?" + direction);
+            }
+
+            //System.out.println("horizontalPosition: " + horizontalPosition + " depth: " + depth + " aim: " + aim);
+        }        
+
+        return depth * horizontalPosition;
     }
 
     public static void main(String[] args) {
