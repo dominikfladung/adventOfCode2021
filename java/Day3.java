594536b1663c1b4bab5f8fd3f1d8e89d0714b5c9
Dominik Fladung <dominik.fladung@fkt42.de>
2021-12-03 20:30:48 +0100

    wip day3

--- java/Day3.java
+++ java/Day3.java
@@ -1,9 +1,30 @@
 public class Day3 extends Day {
-
     @Override
     int part1(String[] puzzleInput) {
-      
-      return 0;
+        int charLength = puzzleInput[0].length();
+        int[] zeros = new int[charLength];
+
+        StringBuilder gammaRate = new StringBuilder();
+        StringBuilder epsilonRate = new StringBuilder();
+
+        for (int i = 0; i < puzzleInput.length; i++) {
+            for (int j = 0; j < charLength; j++) {
+                char number = puzzleInput[i].charAt(j);
+                if (number == '0') {
+                    zeros[j]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < charLength; i++) {
+            if (zeros[i] > Math.floor(puzzleInput.length / 2)) {
+                gammaRate.append("0");
+            } else {
+                gammaRate.append("1");
+            }
+        }
+        
+        return Integer.parseInt(gammaRate.toString(), 2) * Integer.parseInt(epsilonRate.toString(), 2);
     }
 
     @Override
