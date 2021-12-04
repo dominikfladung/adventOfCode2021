52348c5fe1e96816e1cce6c5111191f7b3a6c1de
dominikfladung <dominik.fladung@gmx.de>
2021-12-04 17:04:35 +0100

    refactoring day 4

--- java/Day4.java
+++ java/Day4.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Day4 extends Day {
@@ -11,7 +12,7 @@ public class Day4 extends Day {
     int part1(String[] puzzleInput) {
         Bingo bingo = new Bingo(puzzleInput);
         int i = 0;
-        Field[][] winnerBoard = null;
+        Board winnerBoard = null;
 
         while (winnerBoard == null && i < bingo.bingSubsystem.length) {
             bingo.markFields(bingo.bingSubsystem[i]);
@@ -23,93 +24,102 @@ public class Day4 extends Day {
             return -1;
         }
 
-        return bingo.getScore(winnerBoard, bingo.bingSubsystem[i - 1]);
+        return winnerBoard.getScore(bingo.bingSubsystem[i - 1]);
     }
 
     @Override
     int part2(String[] puzzleInput) {
         Bingo bingo = new Bingo(puzzleInput);
-        int i = 0;
-        List<Integer> winners = new ArrayList<Integer>();
+        int lastWinnerScore = 0;
 
-        while (i < bingo.bingSubsystem.length) {
-            bingo.markFields(bingo.bingSubsystem[i]);
-            Field[][] winnerBoard = bingo.getAndRemoveWinner();
-            
-            if (winnerBoard != null) {
-                winners.add(bingo.getScore(winnerBoard, bingo.bingSubsystem[i]));
+        for (int number : bingo.bingSubsystem) {
+            for (Board board : bingo.boards) {
+                if(!board.won) {
+                    board.markFields(number);
+                    if (board.isWinner()) {
+                        lastWinnerScore = board.getScore(number);
+                        //System.out.println("Winner board: " + board.getScore(number) + " " + board.id);
+                    }
+                  // board.printBoard();
+                }
             }
-
-            i++;
         }
-        
-        return winners.get(winners.size()-1);
+
+        return lastWinnerScore;
     }
 
     private class Bingo {
-        public List<Field[][]> boards = new ArrayList<Field[][]>();
+        public List<Board> boards = new ArrayList<Board>();
         public int[] bingSubsystem;
 
         Bingo(String[] puzzleInput) {
             this.bingSubsystem = toInt(puzzleInput[0].split(","));
             for (int i = 1; i < puzzleInput.length; i += 6) {
-                Field[][] board = toBoard(Arrays.copyOfRange(puzzleInput, i, i + 6));
+                Board board = new Board(Arrays.copyOfRange(puzzleInput, i + 1, i + 6));
                 boards.add(board);
             }
         }
 
-        Field[][] toBoard(String[] input) {
-            Field[][] board = new Field[5][5];
-
-            for (int y = 1; y < 6; y++) {
-                String[] row = filterEmptyRows(input[y].split(" "));
-                int[] numbers = toInt(row);
-                for (int x = 0; x < numbers.length; x++) {
-                    board[y - 1][x] = new Field(numbers[x]);
-                }
-            }
-
-            return board;
-        }
-
         void markFields(int number) {
-            for (Field[][] board : boards) {
-                for (Field[] row : board) {
-                    for (Field field : row) {
-                        field.markIfNumbersMatch(number);
-                    }
-                }
+            for (Board board : boards) {
+                board.markFields(number);
             }
         }
 
-        Field[][] getWinner() {
-            for (Field[][] board : boards) {
-                if (isWinnerBoard(board)) {
+        Board getWinner() {
+            for (Board board : boards) {
+                if (board.isWinner()) {
                     return board;
                 }
             }
 
             return null;
         }
+    }
 
-        Field[][] getAndRemoveWinner() {
-            for (int i = 0; i < boards.size(); i++) {
-                if (isWinnerBoard(boards.get(i))) {
-                    return boards.remove(i);
+    private class Board implements Iterable<Field[]> {
+        public static int BoardCounter = 0;
+        private final Field[][] fields = new Field[5][5];
+        public final int id = BoardCounter++;
+        public boolean won = false;
+
+        Board(String[] input) {
+            for (int y = 0; y < 5; y++) {
+                String[] row = filterEmptyRows(input[y].split(" "));
+                int[] numbers = toInt(row);
+                for (int x = 0; x < numbers.length; x++) {
+                    fields[y][x] = new Field(numbers[x]);
                 }
             }
-
-            return null;
         }
 
-        int getScore(Field[][] board, int lastUsedNumber) {
-            int sum = getSumOfUnmarkedFields(board);
-            return sum * lastUsedNumber;
+        void printBoard() {
+            System.out.println("----------------------" + id + "--------------------------");
+            for (int y = 0; y < 5; y++) {
+                for (int x = 0; x < 5; x++) {
+                    System.out.print((fields[y][x].isMarked ? "X" : fields[y][x].number) + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("---------------------/" + id + "--------------------------");
         }
 
-        int getSumOfUnmarkedFields(Field[][] board) {
+        void markFields(int number) {
+            for (Field[] row : this) {
+                for (Field field : row) {
+                    field.markIfNumbersMatch(number);
+                }
+            }
+        }
+
+        int getScore(int number) {
+            int sum = getSumOfUnmarkedFields();
+            return sum * number;
+        }
+
+        int getSumOfUnmarkedFields() {
             int score = 0;
-            for (Field[] row : board) {
+            for (Field[] row : this) {
                 for (Field field : row) {
                     if (!field.isMarked) {
                         score += field.number;
@@ -120,22 +130,23 @@ public class Day4 extends Day {
             return score;
         }
 
-        boolean isWinnerBoard(Field[][] board) {
+        boolean isWinner() {
             int xCounter = 0;
             int yCounter = 0;
 
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[i].length; j++) {
-                    if (board[i][j].isMarked) {
+            for (int i = 0; i < this.fields.length; i++) {
+                for (int j = 0; j < this.fields[i].length; j++) {
+                    if (this.fields[i][j].isMarked) {
                         xCounter++;
                     }
 
-                    if (board[j][i].isMarked) {
+                    if (this.fields[j][i].isMarked) {
                         yCounter++;
                     }
                 }
 
-                if (xCounter == board[i].length || yCounter == board.length) {
+                if (xCounter == this.fields[i].length || yCounter == this.fields.length) {
+                    this.won = true;
                     return true;
                 }
 
@@ -144,6 +155,24 @@ public class Day4 extends Day {
             }
 
             return false;
+        }
+
+        @Override
+        public Iterator<Day4.Field[]> iterator() {
+            return new Iterator<Field[]>()
+            {
+                private int i = 0;
+     
+                @Override
+                public boolean hasNext() {
+                    return fields.length > i;
+                }
+     
+                @Override
+                public Field[] next() {
+                    return fields[i++];
+                }
+            };
         }
     }
 
