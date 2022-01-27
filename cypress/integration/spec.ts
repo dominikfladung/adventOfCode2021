10e0a5df3d87480ae3bcaa7d6aba7515640dd368
dominikfladung <dominik.fladung@gmx.de>
2022-01-27 20:49:09 +0100

    Fix typo; Add Day4 in Typescript; TODO implement Day4

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,7 +1,8 @@
 import Day1 from "../../typescript/Day1"
-import Day2 from '../../typescript//Day2';
-import Day3 from '../../typescript//Day3';
-import { Day } from '../../typescript//BaseDay';
+import Day2 from '../../typescript/Day2';
+import Day3 from '../../typescript/Day3';
+import Day4 from '../../typescript/Day4';
+import { Day } from '../../typescript/BaseDay';
 
 class DaySolution {
     day: Day
@@ -21,22 +22,21 @@ testDays.push(
     new DaySolution(new Day1(), "1711", "1743"),
     new DaySolution(new Day2(), "2091984", "2086261056"),
     new DaySolution(new Day3(), "3009600", "6940518"),
+    new DaySolution(new Day4(), "4662", "12080"),
 );
 
 for (let i = 1; i <= testDays.length; i++) {
     describe(`Day ${i}`, () => {
         const testDay = testDays[i - 1];
         
-        it('Part1', () => {
-            testDay.day.runPart1(i).then(result => {
-                expect(result).to.equal(testDay.part1Solution)
-            });
+        it('Part1', async () => {
+            const result = await testDay.day.runPart1(i)
+            expect(result).to.equal(testDay.part1Solution)
         })
 
-        it('Part2', () => {
-            testDay.day.runPart2(i).then(result => {
-                expect(result).to.equal(testDay.part2Solution)
-            });
+        it('Part2', async () => {
+            const result = await testDay.day.runPart2(i)
+            expect(result).to.equal(testDay.part2Solution)
         })
     })
 }
---
+++ typescript/Day4.ts
@@ -0,0 +1,12 @@
+import { BaseDay } from "./BaseDay";
+
+class Day4 extends BaseDay {
+    part1(puzzleInput: string[]): string {
+        return "1";
+    }
+    part2(puzzleInput: string[]): string {
+        return "2";
+    }
+}
+
+export default Day4;
